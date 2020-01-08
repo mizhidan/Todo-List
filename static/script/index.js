0c092b353981734bce7a6cd2806d36d213ec3f16
mizhidan <dj9824@126.com>
2020-01-08 13:28:08 +0800

    [danjie] {feat}-{4}: click button show corresponding todos.

--- static/script/index.js
+++ static/script/index.js
@@ -22,7 +22,7 @@ function addTodo() {
     const value = {
       id: index,
       content: todoInput.value,
-
+      isFin: false
     }
     dataArr.push(value);
     localStorage.setItem("todoList",JSON.stringify(dataArr));
@@ -30,11 +30,12 @@ function addTodo() {
 }
 
 function initList() {
-  item = JSON.parse(localStorage.getItem("todoList"));
+  let item = JSON.parse(localStorage.getItem("todoList"));
+  todoList.innerHTML = "";
   for(let index = 0; index < item.length; ++index) {
     var todoItem = document.createElement("li");
     todoItem.innerHTML = `
-    <input type="checkbox" name="check-item" /><span>${item[index].content}</span>
+    <input type="checkbox" name="check-item" ${item[index].isFin ? 'checked' : ""} onclick = changeState(${item[index].id}) /><span>${item[index].content}</span>
   `
   todoList.appendChild(todoItem);
   }
@@ -44,26 +45,78 @@ function showContent(status) {
   if(todoInput.value === "") {
     return;
   }
-  var todoItem = document.createElement("li");
-  item = JSON.parse(localStorage.getItem("todoList"));
+  todoList.innerHTML = "";
+  let item = JSON.parse(localStorage.getItem("todoList"));
+  if(status === 'active') {
+    showActive()
+  } else if(status === 'complete') {
+    showComplete();
+  } else {
+    initList();
+  }
+  // for(let index = 0; index < item.length; ++index) {
+  //   if(item[index].status === status) {
+  //     var todoItem = document.createElement("li");
+  //     todoItem.innerHTML = `
+  //     <input type="checkbox" name="check-item" /><span>${item[index].content}</span>
+  //   `
+  //   todoList.appendChild(todoItem);
+  //   }
+  // }
+}
+
+function showActive() {
+  let item = JSON.parse(localStorage.getItem("todoList"));
+  todoList.innerHTML = "";
   for(let index = 0; index < item.length; ++index) {
-    todoItem.innerHTML = `
-    <input type="checkbox" name="check-item" /><span>${item[index].content}</span>
-  `
-  todoList.appendChild(todoItem);
+    if(!item[index].isFin) {
+      var todoItem = document.createElement("li");
+      todoItem.innerHTML = `
+      <input type="checkbox" name="check-item" onclick = changeState(${item[index].id}) /><span>${item[index].content}</span>
+    `
+    todoList.appendChild(todoItem);
+    }
   }
 }
 
-function addItemAndShow(status) {
+function showComplete() {
+  let item = JSON.parse(localStorage.getItem("todoList"));
+  todoList.innerHTML = "";
+  for(let index = 0; index < item.length; ++index) {
+    if(item[index].isFin) {
+      var todoItem = document.createElement("li");
+      todoItem.innerHTML = `
+      <input type="checkbox" name="check-item" ${item[index].isFin ? 'checked' : ""} onclick = changeState(${item[index].id}) /><span>${item[index].content}</span>
+    `
+    todoList.appendChild(todoItem);
+    }
+  }
+}
+
+function addItemAndShow() {
   addTodo();
-  showContent(status);
+  showContent();
+}
+
+function changeState(index) {
+  let dataArr = JSON.parse(localStorage.getItem("todoList"));
+  let item = 0;
+  while(index !== dataArr[item].id) {
+      item++;
+  }
+  dataArr[item].isFin = !dataArr[item].isFin;
+  localStorage.setItem("todoList", JSON.stringify(dataArr));
 }
 
 statusButton.addEventListener("click", event => {
   let target = event.target;
+  let item = JSON.parse(localStorage.getItem("todoList"));
   switch (target.name) {
+    case "check-item":
+      changeState(target);
+      break;
     case "all-btn":
-      showContent("all");
+      initList();
       break;
     case "active-btn":
       showContent("active");
